3017d8bd89b9b76cacaa0404f2806827f289651f
Ammar <ammar.badwan95@gmail.com>
2020-04-04 18:10:18 +0300

    finished the copying and the deployment

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # lab-05
-Lab: 05 - Deploy &amp; Extend Your Portfolio
+
+* In this lab I will be deploying my repository to heroku and then add my portfolio to it
+
+* Live link for the [Portfolio](https://ammarb-portfolio.herokuapp.com/)
+
+
+
+## Number and name of feature: #1: Deployment
+
+Estimate of time needed to complete: 1 Hours
+
+Start time: 4:00 pm
+
+Finish time: 5:00 pm
+
+Actual time needed to complete: 1 hour
+
+## Number and name of feature: #2: Add functionality
+
+Estimate of time needed to complete: 1 Hours
+
+Start time: 5:00 pm
+
+Finish time: 6:00 pm
+
+Actual time needed to complete: 1 hour
--- server.js
+++ server.js
@@ -6,9 +6,13 @@ app.use(express.static('./public'));
 app.get('/hello',(request,response)=>{
     response.status(200).send('Hello!');
 })
-app.get('/data',(request,response)=>{
-let data = [{name: 'C#'},{name: 'Javascript'},{name:'Python'}];
-response.status(200).json(data);
-})
+app.get('/data', (request, response) => {
+    let airplanes = {
+      departure: Date.now(),
+      canFly: true,
+      pilot: 'Well Trained',
+    };
+    response.status(200).json(airplanes);
+  });
 app.get ('*',(request,response)=> response.status(400).send('Error 404:Page not found.'))
 app.listen(PORT, ()=> console.log(`listening to port ${PORT}`));
